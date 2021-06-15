e3423ae869dc14caabac1573933b0288e59ce437
Icaro Peretti <icaroperetti50@gmail.com>
2021-06-15 13:19:09 -0300

    Desenhando pontos com o clique do mouse e alterando cor com teclas especiais

---
+++ MouseDrawing.cpp
@@ -0,0 +1,130 @@
+#include <stdlib.h>
+#include <GL/glut.h>
+#include <math.h>
+#define WINDOW_HEIGHT 400
+#define WINDOW_WIDTH 400
+
+void keyboard(int key, int x, int y);
+void resize(GLsizei w, GLsizei h);
+void display(void);
+void mouse(int button, int state, int mousex, int mousey);
+
+float x, y,red,green,blue;
+
+bool check = false;
+
+int main(int argc, char** argv) {
+    // controla se o sistema operacional tem suporte a janelas
+    glutInit(&argc, argv);
+
+    // quantidade de buffer de cores e que padrão de cores é RGB ou RGBA
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+    // tamanho da janela
+    glutInitWindowSize(400, 400);
+
+    // posicao inicial da janela
+    glutInitWindowPosition(100, 100);
+
+    // cria janela
+    glutCreateWindow("Mouse");
+
+    glutSpecialFunc(&keyboard);
+
+    glutReshapeFunc(&resize);
+
+    glutDisplayFunc(display);
+    
+    glutMouseFunc(mouse);  
+
+    glutMainLoop();
+
+    return EXIT_SUCCESS;
+}
+
+
+void resize(GLsizei w, GLsizei h) {
+    // evite divisao por zero
+    if (h == 0)
+        h = 1;
+    // Especifica as dimensões do Viewport
+    glViewport(0, 0, w, h);
+
+    // inicializa os sistemas de cordernadas
+    glMatrixMode(GL_PROJECTION);
+    // limpa memoria
+    glLoadIdentity();
+
+    if (w <= h) {
+        gluOrtho2D(0.0, WINDOW_WIDTH, 0.0, WINDOW_HEIGHT);
+    }
+    else {
+        gluOrtho2D(0.0, WINDOW_WIDTH, 0.0, WINDOW_HEIGHT);
+    }
+    glMatrixMode(GL_MODELVIEW);
+}
+
+
+void mouse(int button, int state, int mousex, int mousey)
+{
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
+        check = true;
+
+        x = mousex;
+        y = 400 - mousey;
+       
+    }
+    else {
+        check = false;
+    }
+}
+
+void keyboard(int key, int x, int y) {
+    if (key == GLUT_KEY_F1) {
+        red = 1;
+        green = 0;
+        blue = 0;
+    }
+
+    if (key == GLUT_KEY_F2) {
+        red = 0;
+        green = 1;
+        blue = 0;
+    }
+
+    if (key == GLUT_KEY_F3) {
+        red = 0;
+        green = 1;
+        blue = 1;
+    }
+    glutPostRedisplay();
+}
+
+void display() {
+    glColor3f(red, green, blue);
+    glPointSize(50);
+    glEnable(GL_POINT_SMOOTH);
+    glMatrixMode(GL_MODELVIEW);
+    // LIMPAR toda a matriz
+    glLoadIdentity();
+
+    // cor de fundo
+    glClearColor(0, 0, 1, 1);
+    // limpa a tela com a cor de fundo
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    //centralizar plano cartesiano
+    //glTranslatef(WINDOW_HEIGHT / 2, WINDOW_WIDTH / 2, 0);
+
+    if (check)
+    {
+        glBegin(GL_POINTS); // writes pixels on the frame buffer with the current drawing color
+        glVertex2f(x, y);   // sets vertex
+
+        glEnd();
+    }
+  
+
+    glFlush(); // executa o desenho 
+}
